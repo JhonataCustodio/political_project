1b0a79033b74c3a2206ee9d55137108d5bf59683
JhonataCustodio <silvajhonata@live.com>
2023-03-12 16:38:37 +0044

    Test for the main class

--- src/test/java/com/github/jhonatacustodio/challendweekproject/PoliticalprojectApplicationTests.java
+++ src/test/java/com/github/jhonatacustodio/challendweekproject/PoliticalprojectApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PoliticalprojectApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		PoliticalprojectApplication.main(new String[]{});
 	}
 
 }
